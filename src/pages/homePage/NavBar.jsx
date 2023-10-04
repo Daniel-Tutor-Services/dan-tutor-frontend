226ce7a3a4e77466236501efe610451c1ee436b8
Achilihu Daniel Uchenna <achilihudaniel53@gmail.com>
2023-10-04 18:38:29 +0100

    tasks

--- src/pages/homePage/NavBar.jsx
+++ src/pages/homePage/NavBar.jsx
@@ -97,9 +97,9 @@ function NavBar() {
                         )
                     }
                 
-                    <Link to='/contactus' className='links'>
+                    <NavLink to='/contactus' className='links' onFocus={ScrollToTop()} >
                         <li>Contact</li>
-                    </Link> 
+                    </NavLink> 
 
                 </ul>
 
@@ -130,7 +130,7 @@ function NavBar() {
 
                                             <Link to='/profile-page' className='links'> 
                                             <br />
-                                                <h6 style={{fontSize:'0.68em', color:'darkblue', fontWeight:'bolder',margin:'0.4rem', width:'180px', wordBreak:'break-word'}}>{`${userInfo.email}`}</h6>
+                                                <h6 style={{fontSize:'0.68em', color:'#0057ff', fontWeight:'bolder',margin:'0.4rem', width:'180px', wordBreak:'break-word'}}>{`${userInfo.email}`}</h6>
                                             </Link>                                           
                                         </div>
 
